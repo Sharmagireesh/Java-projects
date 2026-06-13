46198895368489494b5872b7f298232ee89e70e0
Gireesh Sharma <gireeshsharma@Gireeshs-MacBook.local>
2026-06-13 07:42:27 +0530

    switch code

---
+++ Switch.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Switch {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter the number between 1-7");
+        int num = sc.nextInt();
+        switch (num){
+            case 1 : System.out.println("Monday");
+            break;
+            case 2 : System.out.println("Tuesday");
+            break;
+            case 3 : System.out.println("Wednesday");
+            break;
+            case 4 : System.out.println("Thursday");
+            break;
+            case 5 : System.out.println("Friday");
+            break;
+            case 6 : System.out.println("Saturday");
+            break;
+            case 7 : System.out.println("Sunday");
+            break;
+            default:
+                System.out.println("Wrong input");
+                break;
+
+        }
+    }
+    
+}
