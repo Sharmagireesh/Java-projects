8cfd035c356934a09aaf4d80965efa6bd910c79a
Gireesh Sharma <147824153+Sharmagireesh@users.noreply.github.com>
2026-07-28 11:07:40 +0530

    Add files via upload

---
+++ SubArrays.java
@@ -0,0 +1,26 @@
+public class SubArrays {     
+    
+    public static int printSubArrays(int numbers[]) {         
+        int ts = 0;         
+        for(int i = 0; i < numbers.length; i++){             
+            int start = i;             
+            for (int j = i; j < numbers.length; j++){                 
+                int end = j;                 
+                for (int k = start; k <= end; k++){
+                    System.out.print(numbers[k] + " "); 
+                }                 
+                ts++;                 
+                System.out.println();             
+            }         
+        }
+        System.out.println();
+        return ts; 
+    }     
+
+    public static void main(String[] args) {         
+        int numbers[] = {2, 4, 6, 8, 10};         
+        
+        int totalSubarrays = printSubArrays(numbers);         
+        System.out.println("total subarrays = " + totalSubarrays);     
+    }      
+}
