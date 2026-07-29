93a9bbd036143225ac3b1cf9af022c37a431b915
Gireesh Sharma <147824153+Sharmagireesh@users.noreply.github.com>
2026-07-29 22:29:37 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int x = sc.nextInt();
+
+        Palindrome solution = new Palindrome();
+
+        if (solution.isPalindrome(x)) {
+            System.out.println(x + " is a Palindrome.");
+        } else {
+            System.out.println(x + " is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
